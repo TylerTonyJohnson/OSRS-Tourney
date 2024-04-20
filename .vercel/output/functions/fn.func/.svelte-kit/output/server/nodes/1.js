e7dfb1acc572e7eba5068e02d478043d79ee6fc4
TannerJohnson <soccerdude2222@hotmail.com>
2024-04-19 18:21:43 -0700

    Making another commit

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BSdWMK9t.js","_app/immutable/chunks/scheduler.B_M5xA-k.js","_app/immutable/chunks/index.8At0uzev.js","_app/immutable/chunks/entry.BS-Jrx4E.js","_app/immutable/chunks/index.DfSpG__o.js"];
+export const imports = ["_app/immutable/nodes/1.CjyGcwE1.js","_app/immutable/chunks/scheduler.B_M5xA-k.js","_app/immutable/chunks/index.8At0uzev.js","_app/immutable/chunks/entry.BGp_XPCb.js","_app/immutable/chunks/index.DfSpG__o.js"];
 export const stylesheets = [];
 export const fonts = [];
---
+++ .vercel/output/static/_app/immutable/entry/start.CNnOwdLa.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BGp_XPCb.js";export{t as start};
